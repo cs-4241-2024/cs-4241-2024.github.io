721b262866f8eae508f736a67b435e5e078f88ae
Glitch (a1-jpolv) <none>
2024-09-04 20:41:10 +0000

    🦺🫐 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require('http'),
+      fs   = require('fs'),
+      port = 3000
+
+const server = http.createServer( function( request,response ) {
+  switch( request.url ) {
+    case '/':
+      sendFile( response, 'index.html' )
+      break
+    case '/index.html':
+      sendFile( response, 'index.html' )
+      break
+    default:
+      response.end( '404 Error: File Not Found' )
+  }
+})
+
+server.listen( process.env.PORT || port )
+
+const sendFile = function( response, filename ) {
+   fs.readFile( filename, function( err, content ) {
+     response.end( content, 'utf-8' )
+   })
+}
